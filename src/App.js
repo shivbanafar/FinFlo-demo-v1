3a189e5cb33c5e260a311af801b8504f2823f104
Shiv Banafar <134961460+shivbanafar@users.noreply.github.com>
2024-07-12 17:27:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Login from './Login';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Login />
+      </main>
+    </div>
+  );
+}
+
+export default App;
